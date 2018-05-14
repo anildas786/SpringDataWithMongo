c0fe8635d089cbe5a52355ead5f8eee92f092410
anil.das <anil.das786@gmail.com>
2018-05-14 20:14:45 +0530

    second commit

---
+++ MongoSchema.sql
@@ -0,0 +1,10 @@
+use productdb
+
+db.createCollection('product')
+
+db.getCollection('product').insert ({"name" : "Mobile 1", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
+db.getCollection('product').insert ({"name" : "Mobile 2", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
+db.getCollection('product').insert ({"name" : "Mobile 3", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
+db.getCollection('product').insert ({"name" : "Mobile 4", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
+db.getCollection('product').insert ({"name" : "Mobile 5", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
+db.getCollection('product').insert ({"name" : "Mobile 6", "price":45.0 ,"quantity": 5, "status": true , "date":ISODate("2018-05-18T00:00:00.000Z") })
--- src/main/java/com/example/demo/SpringDataWithMongoApplication.java
+++ src/main/java/com/example/demo/SpringDataWithMongoApplication.java
@@ -1,12 +1,41 @@
 package com.example.demo;
 
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.entities.Product;
+import com.example.demo.repositories.ProductRepository;
+
 @SpringBootApplication
-public class SpringDataWithMongoApplication {
+public class SpringDataWithMongoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataWithMongoApplication.class, args);
 	}
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@Override
+	public void run(String... args0) throws Exception {
+		
+		SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		for ( Product product: productRepository.findAll()) {
+			
+			System.out.println("Product ID : " + product.getId());
+			System.out.println("Product Name : " + product.getName());
+			System.out.println("Product Price : " + product.getPrice());
+			System.out.println("Product Quantity : " + product.getQuantity());
+			System.out.println("Product Status : " + product.isStatus());
+			System.out.println("Product Date : " + date.format(product.getDate()));
+			System.out.println("*************************");
+			
+		}
+		
+	}
+
 }
